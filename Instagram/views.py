41f362efc884b9961c2b4793303633f94b3bf5b0
Ático <atico@202C11E17168971.local>
2017-11-04 12:54:20 -0500

    Tercer commit

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    else:
+        name = request.POST['nombre_completo']
+        username = request.POST['usuario']
+        email = request.POST['correo']
+        password = request.POST['password']
+        print (name)
+        print (username)
+        print (email)
+        print (password)
+        usuarioDjango = User.objects.create_user(username = username,
+                        password = password, email = email,
+                        first_name = name)
+        miUsuario = MiUsuario( usuario_django = usuarioDjango )
+        usuarioDjango.save()
+        miUsuario.save()
+        return redirect('login')
 
-def login(request):
-    return render(request, 'login.html')
+@login_required
+def home (request):
+    return render(request, 'home.html')
 
-def suma(request):
-    numero1 = request.POST['num1']
-    numero2 = request.POST['num2']
-    resultado = numero1 + numero2
-    context = {'resultado' : resultado}
-    return render (request, resultado, context)
+@login_required
+def profile (request):
+    mi_usuario = MiUsuario.objects.get(pk = request.user.pk)
+    context = {'usuario_actual': mi_usuario}
+    return render(request, 'profile.html', context)
